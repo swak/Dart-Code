147b89879e8313c173f3a7a5fd5c9de566eb76e6
Danny Tuppeny <danny@tuppeny.com>
2018-08-23 11:21:16 +0100

    Run less tests

--- test/dart_only/extension.test.ts
+++ test/dart_only/extension.test.ts
@@ -9,7 +9,7 @@ import { activateWithoutAnalysis, ext, extApi } from "../helpers";
 const sampleFilePath = (isWin ? "X:\\" : "/tmp/") + "sample.dart";
 const sampleFileUri = vs.Uri.parse(`untitled:${sampleFilePath}`);
 
-describe("test environment", () => {
+describe.only("test environment", () => {
 	it("has opened the correct folder", () => {
 		const wfs = vs.workspace.workspaceFolders;
 		assert.equal(wfs.length, 1);
