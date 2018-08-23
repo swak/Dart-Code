4f297a281bc35fa2fc39befa203c92dcb6cef58e
Danny Tuppeny <danny@tuppeny.com>
2018-08-23 14:54:49 +0100

    This?

--- test/test_runner.ts
+++ test/test_runner.ts
@@ -1,22 +1,23 @@
 console.log("Starting test runner...");
 
+import * as fs from "fs";
 import { MultiReporter } from "./mocha_multi_reporter";
 import testRunner = require("vscode/lib/testrunner");
 const onExit = require("signal-exit"); // tslint:disable-line:no-var-requires
 
-// // Ensure we write coverage on exit.
-// declare const __coverage__: any;
-// onExit(() => {
-// 	// Unhandled exceptions here seem to hang, but console.error+process.exit do not! ¯\_(ツ)_/¯
-// 	try {
-// 		if (typeof __coverage__ !== "undefined" && typeof process.env.COVERAGE_OUTPUT !== "undefined" && process.env.COVERAGE_OUTPUT) {
-// 			fs.writeFileSync(process.env.COVERAGE_OUTPUT, JSON.stringify(__coverage__));
-// 		}
-// 	} catch (e) {
-// 		console.error(e);
-// 		process.exit(1);
-// 	}
-// });
+// Ensure we write coverage on exit.
+declare const __coverage__: any;
+onExit(() => {
+	// Unhandled exceptions here seem to hang, but console.error+process.exit do not! ¯\_(ツ)_/¯
+	try {
+		if (typeof __coverage__ !== "undefined" && typeof process.env.COVERAGE_OUTPUT !== "undefined" && process.env.COVERAGE_OUTPUT) {
+			fs.writeFileSync(process.env.COVERAGE_OUTPUT, JSON.stringify(__coverage__));
+		}
+	} catch (e) {
+		console.error(e);
+		//process.exit(1);
+	}
+});
 
 testRunner.configure({
 	forbidOnly: !!process.env.MOCHA_FORBID_ONLY,
