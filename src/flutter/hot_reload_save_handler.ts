eed0d5c257fb2cec0d4e4e32651141f799a96b28
Danny Tuppeny <danny@tuppeny.com>
2018-11-08 18:08:13 +0000

    Disable bits that are handled by LSP

--- src/flutter/hot_reload_save_handler.ts
+++ src/flutter/hot_reload_save_handler.ts
@@ -1,17 +1,18 @@
-import { commands, DiagnosticCollection, DiagnosticSeverity, ExtensionContext, workspace } from "vscode";
+import { commands, DiagnosticSeverity, ExtensionContext, languages, workspace } from "vscode";
 import { config } from "../config";
 import { restartReasonSave } from "../constants";
 import { shouldTriggerHotReload } from "../utils";
 
-export function setUpHotReloadOnSave(context: ExtensionContext, diagnostics: DiagnosticCollection) {
+export function setUpHotReloadOnSave(context: ExtensionContext) {
 	let hotReloadDelayTimer: NodeJS.Timer | undefined;
 	context.subscriptions.push(workspace.onDidSaveTextDocument((td) => {
 		if (!config.flutterHotReloadOnSave || !shouldTriggerHotReload(td))
 			return;
 
 		// Don't do if we have errors for the saved file.
-		const errors = diagnostics.get(td.uri);
-		const hasErrors = errors && errors.find((d) => d.severity === DiagnosticSeverity.Error) != null;
+		// TODO: Test this!!!!!!!!!
+		const errors = languages.getDiagnostics(td.uri);
+		const hasErrors = errors && errors.find((d) => d.source === "dart" && d.severity === DiagnosticSeverity.Error) != null;
 		if (hasErrors)
 			return;
 
