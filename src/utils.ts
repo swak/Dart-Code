ead89c446e92ee2ba8019c629a975b148b80ddac
Danny Tuppeny <danny@tuppeny.com>
2018-11-27 14:12:06 +0000

    Fix which files are sent to LSP server

--- src/utils.ts
+++ src/utils.ts
@@ -141,7 +141,7 @@ export function shouldTriggerHotReload(document: TextDocument): boolean {
 }
 
 export function isAnalyzable(document: TextDocument): boolean {
-	// Keep this in-sync with the LSP setup (fileEvents).
+	// Keep this in-sync with the LSP setup.
 	if (document.isUntitled || !fsPath(document.uri) || document.uri.scheme !== "file")
 		return false;
 
